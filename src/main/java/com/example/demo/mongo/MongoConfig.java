db7dcb8c9b6f9544e382daadc355aa6df2902ddd
Stefan2834 <iosifs617@gmail.com>
2023-12-11 20:08:24 +0200

    main

--- src/main/java/com/example/demo/mongo/MongoConfig.java
+++ src/main/java/com/example/demo/mongo/MongoConfig.java
@@ -3,18 +3,23 @@ package com.example.demo.mongo;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoRepositories(basePackages = "com.exemple.demo")
+@PropertySource("classpath:application.properties")
 public class MongoConfig {
 
+    @Value("${mongodb.connection-url}")
+    private String mongoDbConnectionUrl;
     @Bean
     public MongoTemplate mongoTemplate() {
-        ConnectionString connectionString = new ConnectionString("mongodb+srv://stefan2834:iQLCeUmzaxXPKzBM@cluster0.vlhvegc.mongodb.net/");
+        ConnectionString connectionString = new ConnectionString(mongoDbConnectionUrl);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
